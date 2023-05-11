50d0ec1dfaa10c931e00502deb6995113bb111c2
HamzaAli303 <alihamza.m1999@gmail.com>
2023-05-11 12:41:15 -0600

    practice

---
+++ main.js
@@ -0,0 +1,9 @@
+const catBtn = document.querySelector('#cat')
+const dogBtn = document.querySelector('#dog')
+
+const catHandler = () => alert('The cat name is: Rascal')
+const dogHandler = () => alert('The dog name is: Doug')
+
+
+catBtn.addEventListener('click', catHandler);
+dogBtn.addEventListener('click', dogHandler);
